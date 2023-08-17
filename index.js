b0ce23719f0ee4817107b35bb01bd253388be80a
eduardoreicherts <dudureichert86@gmail.com>
2023-08-16 21:55:56 -0300

    fiz os exercicios da aula 6

---
+++ index.js
@@ -0,0 +1,53 @@
+/*
+exercicio 1
+resposta: vai imprimir 
+10
+10 5
+
+exercicio 2
+resposta: vai imprimir
+10 10 10
+
+
+exercicio 3
+resposta:
+
+let horasDia = prompt("Quantas horas você trabalha por dia?")
+let recebeDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`) 
+
+*/
+
+
+
+//exercicio 1 codigo
+let nome 
+let idade 
+
+console.log (typeof nome)
+console.log (typeof idade)
+
+// nas duas variaveis deu undefined, por que não nehum valor atribuido
+
+nome = prompt("qual é sue nome")
+idade = prompt("qual é sua idade")
+console.log (typeof nome)
+console.log (typeof idade )
+
+//agora as duas variaveis mudaram para strings
+
+console.log ("olá",nome,", voce tem",idade,"anos.")
+
+//exercicio 2 codigo
+
+let pergunta1 = prompt("VOCE ESTA USANDO ROUPA AZUL HOJE? só responder com sim ou nao")
+let pergunta2 = prompt("VOCE ESTA DE  PE?só responder com sim ou nao")
+let pergunta3 = prompt("VOCE ESTA NA ESCOLA? só responder com sim ou nao")
+
+console.log ("VOCE ESTA USANDO ROUPA AZUL HOJE?", pergunta1)
+console.log ("VOCE ESTA DE PE?", pergunta2)
+console.log ("VOCE ESTA NA ESCOLA?", pergunta3)
+
+//exercicio 3 codigo
+
+
