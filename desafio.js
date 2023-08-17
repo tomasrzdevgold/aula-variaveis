ac58f6a981ed6dd9e561e39ad8fd2a90575a56e6
dt <denersonluz@gmail.com>
2023-08-17 20:06:13 -0300

    adicionei o desafio da aula 6

---
+++ desafio.js
@@ -0,0 +1,7 @@
+let primeiroNumero = prompt("introduça qualquer numero")
+let segundoNumero =  prompt("introduça o segundo numero")
+
+
+
+console.log(primeiroNumero + segundoNumero)
+console.log(primeiroNumero)
